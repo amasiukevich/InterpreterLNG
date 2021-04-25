21c6da330bf7639e4c1ee93cf106d4ca8647cd49
Anton <anton.masiukiewicz1@gmail.com>
2021-04-26 01:30:21 +0200

    Added source tests

--- tests/data_source/test_file_source.py
+++ tests/data_source/test_file_source.py
@@ -1,8 +1,28 @@
-from src.data_source.file_source import FileSource
+from src.data_source.string_source import BaseSource
+from src.data_source.string_source import StringSource
 import unittest
 
 
 class TestFileSource(unittest.TestCase):
 
-    def test_file_source_greeting(self):
-        self.assertEqual(FileSource().greetings(), "Hello from the FileSource")
+    def test_file_source_getting(self):
+
+        string = "This house is not for sale"
+        ss = StringSource(string)
+        symbols = []
+        c = ss.get()
+        while c != BaseSource.EOF:
+            symbols.append(c)
+            c = ss.get()
+
+        self.assertListEqual(symbols, [c for c in string])
+
+
+    def test_source_pointing_first(self):
+
+        string = "Those houses are for sale"
+
+        ss = StringSource(string)
+
+        c = ss.get()
+        self.assertEqual(c, "T")
